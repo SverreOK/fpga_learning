354752db0e25ffa2b6ec853f367ed72c6cc60edc
Sverre <sverre.o.k@hotmail.no>
2023-03-05 14:17:17 +0100

    working :D

--- intern_kurs/Exercise2_Storage/storage.vhd
+++ intern_kurs/Exercise2_Storage/storage.vhd
@@ -19,6 +19,7 @@ architecture rtl of storage is
     type t_STORAGE is array (0 to 3) of std_logic_vector(7 downto 0);
     --  Declare a signal of type t_STORAGE and initialize it to all zeros.
     signal STORAGE : t_STORAGE := (others => (others => '0'));
+    signal prev_CLK : std_logic := '0';
 
     begin
         -- Sensitive to CLK and RST input
@@ -26,7 +27,10 @@ architecture rtl of storage is
         begin 
             if RST = '0' then
                 STORAGE <= (others => (others => '0'));
-            elsif rising_edge(SYSCLK) then
+            end if;
+            if rising_edge(SYSCLK) then
+                prev_CLK <= CLK;
+                if CLK = '1' and prev_CLK = '0' then
                 if ADR(2) = '1' then
                     case ADR(1 downto 0) is
                         when "00" =>
@@ -38,9 +42,10 @@ architecture rtl of storage is
                         when "11" =>
                             READDATA <= STORAGE(3);
                         when others =>
-                            READDATA <= (others => '0');
+                            null;
                     end case;
-                elsif ADR(2) = '0' then
+                end if;
+                if ADR(2) = '0' then
                     case ADR(1 downto 0) is
                         when "00" =>
                             STORAGE(0) <= WRITEDATA;
@@ -53,6 +58,7 @@ architecture rtl of storage is
                         when others =>
                             null;
                     end case;
+                end if;
                 end if;
             end if;
         end process p_READ_nWRITE;
